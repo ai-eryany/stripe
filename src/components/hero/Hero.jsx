cf5bdbcc0a5100e00659616116553b7d45ab921d
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2023-02-09 22:36:10 +0200

    Update: hero media query added and fixed few bugs

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,34 +8,32 @@ import './Hero-media.scss';
 function Hero() {
     return (
         <div className="hero">
-            <div className="hero-content">
-                <div className="hero-title-container">
-                    <h1 className="title title-main ">
-                        Payments infrastructure <br />
-                        for the internet
-                    </h1>
-                    <p className="title title__second">
-                        Payments infrastructure <br />
-                        for the internet
-                    </p>
-                    <p className="title title__overlay">
-                        Payments infrastructure <br />
-                        for the internet
-                    </p>
-                </div>
+            <div className="hero-title-container">
+                <h1 className="title title-main ">
+                    Payments infrastructure <br />
+                    for the internet
+                </h1>
+                <p className="title title__second">
+                    Payments infrastructure <br />
+                    for the internet
+                </p>
+                <p className="title title__overlay">
+                    Payments infrastructure <br />
+                    for the internet
+                </p>
+            </div>
 
-                <div className="hero-desc-container">
-                    <p className="desc">
-                        Millions of businesses of all sizes—from startups to large enterprises—use
-                        Stripe’s software and APIs to accept payments, send payouts, and manage
-                        their businesses online.
-                    </p>
-                </div>
+            <div className="hero-desc-container">
+                <p className="desc">
+                    Millions of businesses of all sizes—from startups to large enterprises—use
+                    Stripe’s software and APIs to accept payments, send payouts, and manage their
+                    businesses online.
+                </p>
+            </div>
 
-                <div className="hero-btn-container">
-                    <button className="btn1">Start now &gt;</button>
-                    <button className="btn2">Contact sales &gt;</button>
-                </div>
+            <div className="hero-btn-container">
+                <button className="btn1">Start now &gt;</button>
+                <button className="btn2">Contact sales &gt;</button>
             </div>
 
             <div className="hero-pictures">
