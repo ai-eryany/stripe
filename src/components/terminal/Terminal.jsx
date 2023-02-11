6c089fd8a908d62eaa9763f88718a2114b4ab1d0
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2023-02-11 18:15:37 +0200

    Update: Terminal component is finished

--- src/components/terminal/Terminal.jsx
+++ src/components/terminal/Terminal.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ide from '../../assets/images/ide.png';
+import gear from '../../assets/icons/gear.svg';
+import tab from '../../assets/icons/tab.svg';
+import animatedBox from '../../assets/icons/animated-box.svg';
+import html from '../../assets/icons/html.svg';
 
 import './Terminal.scss';
 import './Terminal-media.scss';
@@ -7,35 +11,42 @@ import './Terminal-media.scss';
 function Terminal() {
     return (
         <section className="terminal">
-            <div className="terminal-background">
-                <div className="short"></div>
-                <div className="tall"></div>
+            <div className="terminal-lines">
+                <div className="tall">
+                    <div className="short"></div>
+                </div>
             </div>
 
             <div className="terminal-content">
-                <span className="terminal-head">Designed for developers</span>
-
-                <h1 className="terminal-title">
-                    Ship more quickly with powerful and easy-to-use APIs
-                </h1>
-
-                <p className="terminal-desc">
-                    Save engineering time with unified payments functionality.
-                    We obsess over the maze of gateways, payments rails, and
-                    financial institutions that make up the global economic
-                    landscape so that your teams can build what you need on one
-                    platform.
-                </p>
-
-                <button className="terminal-btn">Read the docs &gt;</button>
-
-                <div className="terminal-pic">
-                    <img src={ide} alt="terminal" />
+                <div className="terminal-upper">
+                    <div className="upper-text">
+                        <span className="terminal-head">
+                            Designed for developers
+                        </span>
+                        <h1 className="terminal-title">
+                            Ship more quickly with powerful and easy-to-use APIs
+                        </h1>
+                        <p className="terminal-desc">
+                            Save engineering time with unified payments
+                            functionality. We obsess over the maze of gateways,
+                            payments rails, and financial institutions that make
+                            up the global economic landscape so that your teams
+                            can build what you need on one platform.
+                        </p>
+                        <button className="terminal-btn">
+                            Read the docs &gt;
+                        </button>
+                    </div>
+                    <div className="terminal-pic">
+                        <img src={ide} alt="terminal" />
+                    </div>
                 </div>
 
                 <div className="terminal-details">
                     <div className="box">
-                        <img className="anim" src="" alt="gear" />
+                        <div className="shrid">
+                            <img src={gear} alt="gear" />
+                        </div>
                         <h4 className="head">Use Stripe with your stack</h4>
                         <p className="desc">
                             We offer client and server libraries in everything
@@ -45,7 +56,9 @@ function Terminal() {
                     </div>
 
                     <div className="box">
-                        <img className="anim" src="" alt="tab" />
+                        <div className="anim">
+                            <img src={tab} alt="tab" />
+                        </div>
                         <h4 className="head">Try no-code options</h4>
                         <p className="desc">
                             Customize and deploy payments interfaces directly
@@ -54,8 +67,10 @@ function Terminal() {
                         <button className="btn">Explore no-code &gt;</button>
                     </div>
 
-                    <div>
-                        <img className="anim" src="" alt="animated-box" />
+                    <div className="box">
+                        <div className="shrid">
+                            <img src={animatedBox} alt="animated-box" />
+                        </div>
                         <h4 className="head">Explore prebuilt integrations</h4>
                         <p className="desc">
                             Integrate with systems including Adobe, Salesforce,
@@ -66,8 +81,10 @@ function Terminal() {
                         <button className="btn">View connectors &gt;</button>
                     </div>
 
-                    <div>
-                        <img className="anim" src="" alt="html" />
+                    <div className="box">
+                        <div className="anim">
+                            <img src={html} alt="html" />
+                        </div>
                         <h4 className="head">Build an app on Stripe</h4>
                         <p className="desc">
                             Create a custom backend integration or interface
