e4e08717aa27e4c895e75d08fc5f1382957b82d0
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2023-02-12 17:00:07 +0200

    Update: scale

--- src/components/scale/Scale.jsx
+++ src/components/scale/Scale.jsx
@@ -6,9 +6,56 @@ import './Scale-media.scss';
 function Scale() {
     return (
         <section className="scale">
-            <h1 className="scale-title">Scale Here</h1>
-            <div className="globe-container">
+            <div className="scale-lines">
+                <div className="tall">
+                    <div className="short"></div>
+                </div>
+            </div>
+            {/* <div className="globe-container">
                 <div id="globe"></div>
+            </div> */}
+            <div className="scale-content">
+                <span className="scale-head">Global scale</span>
+
+                <div className="scale-title">
+                    <h2>The backbone for global commerce</h2>
+                </div>
+
+                <p className="scale-desc">
+                    Stripe makes moving money as easy and programmable as moving
+                    data. Our teams are based in offices around the world and we
+                    process hundreds of billions of euros each year for
+                    ambitious businesses of all sizes.
+                </p>
+
+                <div className="scale-details">
+                    <div className="box">
+                        <h4 className="head">250M+</h4>
+                        <p className="desc">
+                            API requests per day, peaking at 13,000 requests a
+                            second
+                        </p>
+                    </div>
+
+                    <div className="box">
+                        <h4 className="head">99.999%</h4>
+                        <p className="desc">
+                            historical uptime for stripe services.
+                        </p>
+                    </div>
+
+                    <div className="box">
+                        <h4 className="head">47+</h4>
+                        <p className="desc">countires with local aquiring.</p>
+                    </div>
+
+                    <div className="box">
+                        <h4 className="head">135+</h4>
+                        <p className="desc">
+                            currencies and payment methods supported.
+                        </p>
+                    </div>
+                </div>
             </div>
         </section>
     );
